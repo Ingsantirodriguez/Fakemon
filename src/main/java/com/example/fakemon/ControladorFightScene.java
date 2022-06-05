d5bbbb6be4334d99a56542d10b406f619fe4185b
ginos1998 <ginosiam1998@gmail.com>
2022-06-05 18:56:29 -0300

    V 0.0.9 | fightScene: muestra fakemon elegido y oponente, elegido aleatoriamente

--- src/main/java/com/example/fakemon/ControladorFightScene.java
+++ src/main/java/com/example/fakemon/ControladorFightScene.java
@@ -6,17 +6,40 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
-
+import static com.example.fakemon.DatosConfig.*;
+import com.example.fakemon.DatosConfig.*;
 public class ControladorFightScene implements Initializable {
+
+    public ImageView userFakemon;
+    public ImageView botFakemon;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        selecRandomFakemon();
+        String usfakemon= "src/main/resources/com/example/fakemon/images/"+fakemonElegido+".png";
+        String botfakemon= "src/main/resources/com/example/fakemon/images/"+fakemonOponente+".png";
+        Path imageFile = Paths.get(usfakemon);
+        try {
+            userFakemon.setImage(new Image(imageFile.toUri().toURL().toExternalForm()));
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        Path imgFile = Paths.get(botfakemon);
+        try {
+            botFakemon.setImage(new Image(imgFile.toUri().toURL().toExternalForm()));
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void config(ActionEvent event) throws IOException {
