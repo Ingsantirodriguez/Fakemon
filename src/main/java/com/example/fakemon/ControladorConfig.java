0f6acecad0a984af575f81e193eb40a2c89c54d6
agusc <agustin.chazarreta@mi.unc.edu.ar>
2022-06-05 15:34:58 -0300

    V 0.1.2 - Funcionan escenas, sonido y seleccion de personajes

---
+++ src/main/java/com/example/fakemon/ControladorConfig.java
@@ -0,0 +1,102 @@
+package com.example.fakemon;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.layout.Background;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static com.example.fakemon.DatosConfig.*;
+
+
+public class ControladorConfig implements Initializable {
+
+
+    public CheckBox config1;
+    public TextField Nombre;
+    public CheckBox config2;
+    public CheckBox config3;
+    public Slider VolumenSeleccion;
+    public ToggleButton GeneroToogle;
+    public Button NombreBtn;
+
+
+
+    //accion ir al inicio.fxml
+    public void inicio(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("inicio.fxml"));
+        Scene scene = new Scene(root, Color.WHITE);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    //guardar nombre
+    public void guardarNombre(ActionEvent event) {
+
+        nombre = Nombre.getText();
+        System.out.println(nombre);
+    }
+
+    //cambiar volumen
+    public void cambiarVolumen(ActionEvent event) {
+        volumen = (int) VolumenSeleccion.getValue();
+        System.out.println(volumen);
+    }
+    //cambiar genero
+    public void cambiarGenero(ActionEvent event) {
+        if (GeneroToogle.isSelected()) {
+            Masculino();
+        } else {
+            Femenino();
+            //GeneroToogle texto rojo oscuro y fondo amarillo claro
+             }
+    }
+private void Masculino(){
+    GeneroToogle.setText("Masculino");
+    //GeneroToogle texto azul oscuro y fondo naranja claro
+    GeneroToogle.setTextFill(Color.BLUE);
+    GeneroToogle.setBackground(new Background(new BackgroundFill(Color.rgb(220,179,31),null,null)));
+    genero = "Masculino";
+}
+private void Femenino(){
+    GeneroToogle.setText("Femenino");
+    //GeneroToogle texto azul oscuro y fondo naranja claro
+    GeneroToogle.setTextFill(Color.PURPLE);
+    GeneroToogle.setBackground(new Background(new BackgroundFill(Color.rgb(255,255,128),null,null)));
+    genero = "Femenino";
+}
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        VolumenSeleccion.setValue(volumen);
+        if(!nombre.equals("")){
+            Nombre.setText(nombre);
+        }
+        //texto en genero en negrita
+        GeneroToogle.setStyle("-fx-font-weight: bold");
+
+        //imprimir genero
+        System.out.println(genero);
+        if(genero.equals("Masculino")){
+            Masculino();
+        }
+        else{
+            Femenino();
+        }
+        //setear volumen
+        VolumenSeleccion.setValue(volumen);
+    }
+}
---
+++ src/main/java/com/example/fakemon/ControladorSeleccionPersonaje.java
@@ -0,0 +1,72 @@
+package com.example.fakemon;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControladorSeleccionPersonaje implements Initializable {
+    public ImageView Bulbasaur;
+    public ImageView Pikachu;
+    public ImageView Charmander;
+    public ImageView Pidgey;
+    public ImageView Squirtle;
+    public ImageView Jigglypuff;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void config(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("fightScene.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Seleccionaste Bulbasaur
+    public void Bulbasaur(ActionEvent event) throws IOException {
+    System.out.println("Bulbasaur");
+    config( event);
+    }
+    //Seleccionaste Pikachu
+    public void Pikachu(ActionEvent event) throws IOException {
+        System.out.println("Pikachu");
+        config(event);
+    }
+    //Seleccionaste Charmander
+    public void Charmander(ActionEvent event) throws IOException {
+        System.out.println("Charmander");
+        config(event);
+    }
+    //Seleccionaste Pidgey
+    public void Pidgey(ActionEvent event) throws IOException {
+        System.out.println("Pidgey");
+        config(event);
+    }
+    //Seleccionaste Squirtle
+    public void Squirtle(ActionEvent event) throws IOException {
+        System.out.println("Squirtle");
+        config(event);
+    }
+    //Seleccionaste Jigglypuff
+    public void Jigglypuff(ActionEvent event) throws IOException {
+        System.out.println("Jigglypuff");
+        config(event);
+    }
+
+
+
+
+
+}
