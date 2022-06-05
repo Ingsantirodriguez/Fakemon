1aef78b2c1a5498c852be18a2467299d4de00ac8
ginos1998 <ginosiam1998@gmail.com>
2022-06-05 18:56:29 -0300

    V 0.0.9 | fightScene: muestra fakemon elegido y oponente, elegido aleatoriamente

--- src/main/java/com/example/fakemon/ControladorSeleccionPersonaje.java
+++ src/main/java/com/example/fakemon/ControladorSeleccionPersonaje.java
@@ -8,10 +8,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import static com.example.fakemon.DatosConfig.*;
 
 public class ControladorSeleccionPersonaje implements Initializable {
     public ImageView Bulbasaur;
@@ -23,7 +23,8 @@ public class ControladorSeleccionPersonaje implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        emptyFakemons();
+        fillFakemons();
     }
 
     public void config(ActionEvent event) throws IOException {
@@ -43,31 +44,37 @@ public class ControladorSeleccionPersonaje implements Initializable {
     //Seleccionaste Bulbasaur
     public void Bulbasaur(ActionEvent event) throws IOException {
     System.out.println("Bulbasaur");
-    config( event);
+    fakemonElegido = "Bulbasaur";
+    config(event);
     }
     //Seleccionaste Pikachu
     public void Pikachu(ActionEvent event) throws IOException {
         System.out.println("Pikachu");
+        fakemonElegido = "Pikachu";
         config(event);
     }
     //Seleccionaste Charmander
     public void Charmander(ActionEvent event) throws IOException {
         System.out.println("Charmander");
+        fakemonElegido = "Charmander";
         config(event);
     }
     //Seleccionaste Pidgey
     public void Pidgey(ActionEvent event) throws IOException {
         System.out.println("Pidgey");
+        fakemonElegido = "Pidgey";
         config(event);
     }
     //Seleccionaste Squirtle
     public void Squirtle(ActionEvent event) throws IOException {
         System.out.println("Squirtle");
+        fakemonElegido = "Squirtle";
         config(event);
     }
     //Seleccionaste Jigglypuff
     public void Jigglypuff(ActionEvent event) throws IOException {
         System.out.println("Jigglypuff");
+        fakemonElegido = "Jigglypuff";
         config(event);
     }
 
