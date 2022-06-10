83fae669f60fe18086c68d0d3c560fdef80bdc91
ginos1998 <ginosiam1998@gmail.com>
2022-06-10 14:53:45 -0300

    V 0.0.1 - TorneoTest: getInstance()

---
+++ src/test/java/com/example/fakemon/fakemons/TorneoTest.java
@@ -0,0 +1,16 @@
+package com.example.fakemon.fakemons;
+
+import com.example.fakemon.batalla.Batalla;
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+
+public class TorneoTest extends TestCase {
+    private Batalla batalla = new Batalla();    // Torneo es abstract, pero Batalla hereda
+
+    @Test
+    void instanciaBatalla(){
+        String test = "BATALLA N° 1";
+        assertEquals(batalla.getInstance(), test);
+
+    }
+}
